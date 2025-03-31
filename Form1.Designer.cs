85a1104b3d4717577d4b2e3336b815b1c6630867
Anton Malezhik <Anton Malezhik@PIANISTPC>
2025-03-31 21:01:15 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Arma3_Fov_Changer_2025
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            GUI_profile_label = new Label();
+            GUI_btn_filechoose = new Button();
+            GUI_groupbox = new GroupBox();
+            GUI_label_link_1 = new LinkLabel();
+            GUI_label_link = new LinkLabel();
+            GUI_combo_screen = new ComboBox();
+            GUI_combo_fov = new ComboBox();
+            GUI_checkbox_bakup = new CheckBox();
+            GUI_btn_save = new Button();
+            GUI_label_1 = new Label();
+            GUI_label_0 = new Label();
+            openFileDialog = new OpenFileDialog();
+            pictureBox1 = new PictureBox();
+            GUI_groupbox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // GUI_profile_label
+            // 
+            GUI_profile_label.AutoSize = true;
+            GUI_profile_label.Location = new Point(104, 26);
+            GUI_profile_label.Name = "GUI_profile_label";
+            GUI_profile_label.Size = new Size(213, 15);
+            GUI_profile_label.TabIndex = 0;
+            GUI_profile_label.Text = "Выберите ваш профиль .arma3profile";
+            // 
+            // GUI_btn_filechoose
+            // 
+            GUI_btn_filechoose.Location = new Point(23, 20);
+            GUI_btn_filechoose.Name = "GUI_btn_filechoose";
+            GUI_btn_filechoose.Size = new Size(75, 26);
+            GUI_btn_filechoose.TabIndex = 1;
+            GUI_btn_filechoose.Text = "Выбрать";
+            GUI_btn_filechoose.UseVisualStyleBackColor = true;
+            GUI_btn_filechoose.Click += GUI_btn_filechoose_Click;
+            // 
+            // GUI_groupbox
+            // 
+            GUI_groupbox.BackColor = Color.Transparent;
+            GUI_groupbox.Controls.Add(GUI_label_link_1);
+            GUI_groupbox.Controls.Add(GUI_label_link);
+            GUI_groupbox.Controls.Add(GUI_combo_screen);
+            GUI_groupbox.Controls.Add(GUI_combo_fov);
+            GUI_groupbox.Controls.Add(GUI_checkbox_bakup);
+            GUI_groupbox.Controls.Add(GUI_btn_save);
+            GUI_groupbox.Controls.Add(GUI_label_1);
+            GUI_groupbox.Controls.Add(GUI_label_0);
+            GUI_groupbox.Enabled = false;
+            GUI_groupbox.Location = new Point(23, 67);
+            GUI_groupbox.Name = "GUI_groupbox";
+            GUI_groupbox.Size = new Size(486, 152);
+            GUI_groupbox.TabIndex = 2;
+            GUI_groupbox.TabStop = false;
+            GUI_groupbox.Text = "Параметры";
+            // 
+            // GUI_label_link_1
+            // 
+            GUI_label_link_1.AutoSize = true;
+            GUI_label_link_1.Location = new Point(356, 62);
+            GUI_label_link_1.Name = "GUI_label_link_1";
+            GUI_label_link_1.Size = new Size(94, 15);
+            GUI_label_link_1.TabIndex = 4;
+            GUI_label_link_1.TabStop = true;
+            GUI_label_link_1.Text = "Посетить Itch.io";
+            GUI_label_link_1.LinkClicked += GUI_label_link_1_LinkClicked;
+            // 
+            // GUI_label_link
+            // 
+            GUI_label_link.AutoSize = true;
+            GUI_label_link.Location = new Point(356, 40);
+            GUI_label_link.Name = "GUI_label_link";
+            GUI_label_link.Size = new Size(99, 15);
+            GUI_label_link.TabIndex = 4;
+            GUI_label_link.TabStop = true;
+            GUI_label_link.Text = "Посетить GitHub";
+            GUI_label_link.LinkClicked += GUI_label_link_LinkClicked;
+            // 
+            // GUI_combo_screen
+            // 
+            GUI_combo_screen.DropDownStyle = ComboBoxStyle.DropDownList;
+            GUI_combo_screen.FormattingEnabled = true;
+            GUI_combo_screen.Items.AddRange(new object[] { "7680×4320", "3840×2160", "3100×1440", "2560×1440", "1920×1080", "1366×768", "1280×720", "640×480" });
+            GUI_combo_screen.Location = new Point(20, 56);
+            GUI_combo_screen.Name = "GUI_combo_screen";
+            GUI_combo_screen.Size = new Size(165, 23);
+            GUI_combo_screen.TabIndex = 6;
+            GUI_combo_screen.SelectionChangeCommitted += ComboboxSelectIndex;
+            // 
+            // GUI_combo_fov
+            // 
+            GUI_combo_fov.DropDownStyle = ComboBoxStyle.DropDownList;
+            GUI_combo_fov.FormattingEnabled = true;
+            GUI_combo_fov.Items.AddRange(new object[] { "120", "110", "100", "90", "80", "70" });
+            GUI_combo_fov.Location = new Point(201, 56);
+            GUI_combo_fov.Name = "GUI_combo_fov";
+            GUI_combo_fov.Size = new Size(93, 23);
+            GUI_combo_fov.TabIndex = 6;
+            GUI_combo_fov.SelectionChangeCommitted += ComboboxSelectIndex;
+            // 
+            // GUI_checkbox_bakup
+            // 
+            GUI_checkbox_bakup.AutoSize = true;
+            GUI_checkbox_bakup.Checked = true;
+            GUI_checkbox_bakup.CheckState = CheckState.Checked;
+            GUI_checkbox_bakup.Location = new Point(201, 111);
+            GUI_checkbox_bakup.Name = "GUI_checkbox_bakup";
+            GUI_checkbox_bakup.Size = new Size(254, 19);
+            GUI_checkbox_bakup.TabIndex = 5;
+            GUI_checkbox_bakup.Text = "Создать резерв. копию на рабочем столе";
+            GUI_checkbox_bakup.UseVisualStyleBackColor = true;
+            // 
+            // GUI_btn_save
+            // 
+            GUI_btn_save.Location = new Point(20, 105);
+            GUI_btn_save.Name = "GUI_btn_save";
+            GUI_btn_save.Size = new Size(165, 30);
+            GUI_btn_save.TabIndex = 4;
+            GUI_btn_save.Text = "Сохранить";
+            GUI_btn_save.UseVisualStyleBackColor = true;
+            GUI_btn_save.Click += GUI_btn_save_Click;
+            // 
+            // GUI_label_1
+            // 
+            GUI_label_1.AutoSize = true;
+            GUI_label_1.ForeColor = Color.Gray;
+            GUI_label_1.Location = new Point(201, 33);
+            GUI_label_1.Name = "GUI_label_1";
+            GUI_label_1.Size = new Size(75, 15);
+            GUI_label_1.TabIndex = 3;
+            GUI_label_1.Text = "Угол обзора";
+            // 
+            // GUI_label_0
+            // 
+            GUI_label_0.AutoSize = true;
+            GUI_label_0.ForeColor = Color.Gray;
+            GUI_label_0.Location = new Point(20, 33);
+            GUI_label_0.Name = "GUI_label_0";
+            GUI_label_0.Size = new Size(116, 15);
+            GUI_label_0.TabIndex = 3;
+            GUI_label_0.Text = "Разрешение экрана";
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(413, 1);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(96, 70);
+            pictureBox1.TabIndex = 3;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(529, 238);
+            Controls.Add(GUI_groupbox);
+            Controls.Add(GUI_btn_filechoose);
+            Controls.Add(GUI_profile_label);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Arma 3 Настройка fov 2025";
+            Shown += Form1_Shown;
+            GUI_groupbox.ResumeLayout(false);
+            GUI_groupbox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label GUI_profile_label;
+        private Button GUI_btn_filechoose;
+        private GroupBox GUI_groupbox;
+        private OpenFileDialog openFileDialog;
+        private Button GUI_btn_save;
+        private CheckBox GUI_checkbox_bakup;
+        private ComboBox GUI_combo_fov;
+        private Label GUI_label_0;
+        private ComboBox GUI_combo_screen;
+        private Label GUI_label_1;
+        private PictureBox pictureBox1;
+        private LinkLabel GUI_label_link;
+        private LinkLabel GUI_label_link_1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Diagnostics;
+using System.Security.Policy;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+namespace Arma3_Fov_Changer_2025
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Публичная глобальная переменная с путем профиля
+        public string global_arma3profile_file_path = "";
+
+        // Нажатие на кнопку выбора файла
+        private void GUI_btn_filechoose_Click(object sender, EventArgs e)
+        {
+
+            // Блокировака параметров (Защита от дурака)
+            GUI_groupbox.Enabled = false;
+
+            // Сбрасываем переменную пути
+            global_arma3profile_file_path = "";
+
+            // Сбрасываем текст
+            GUI_profile_label.Text = "Выберите ваш профиль.arma3profile";
+
+            // Открытие диалога выбора файла
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+
+                // Настройки диалогового окна
+                openFileDialog.Title = "Выберите файл профиля Arma 3";
+                openFileDialog.Filter = "Arma 3 Profile Files (*.Arma3Profile)|*.Arma3Profile";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                // Если файл выбран
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Получаем выбранный файл
+                    global_arma3profile_file_path = openFileDialog.FileName;
+
+                    // Меняем текст выбора файла
+                    GUI_profile_label.Text = Uri.UnescapeDataString(Path.GetFileName(global_arma3profile_file_path));
+
+                    // ======================================
+
+                    // Строчки необходимые для поиска в файле
+                    string[] searchLines = { "fovTop=", "fovLeft=" };
+
+                    // Читаем весь файл
+                    string content = File.ReadAllText(global_arma3profile_file_path);
+
+                    // Определяет, были ли найдены сразу две строки или нет
+                    bool allLinesFound = true;
+
+                    // Потерянные строки
+                    string missingLines = "";
+
+                    // Перебираем все строчки
+                    foreach (var line in searchLines)
+                    {
+
+                        // Если строчка не содержанала нужное слово
+                        if (!content.Contains(line))
+                        {
+
+                            // Обновляем статусы
+                            allLinesFound = false;
+                            missingLines += line + "\n";
+                        }
+                    }
+
+                    // Файл не верен и не содержит строчки для изменения FOV
+                    if (!allLinesFound)
+                    {
+
+                        // Уведомление пользователя
+                        MessageBox.Show("К сожалению вы выбрали не тот файл профиля, убедитесь что выбераете файл профиля без .eden и .vars в названии", "Неверный файл профиля", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                    }
+
+                    // Все хорошо, строчки найдены, fov возможно изменить
+                    else
+                    {
+
+                        // Запись в хранилище
+                        Properties.Settings.Default.savedPath = global_arma3profile_file_path; Properties.Settings.Default.Save();
+
+                        // Разблокировка параметров
+                        GUI_groupbox.Enabled = Enabled;
+
+                    }
+                }
+            }
+        }
+
+        // Процедура кулькулирования результата и сохранение его в файл профиля
+        private async Task calculateAndSave(string global_arma3profile_file_path)
+        {
+
+            // Переменные для записи в файл
+            string fovTop, fovLeft;
+
+            // Создаем словарь для хранения значений FOV для каждого разрешения
+            Dictionary<string, Dictionary<string, (string fovTop, string fovLeft)>> fovValues = new()
+            {
+                ["7680×4320"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["3840×2160"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["3100×1440"] = new() {
+                    ["120"] = ("1.07", "2.31"),
+                    ["110"] = ("0.88", "1.90"),
+                    ["100"] = ("0.74", "1.59"),
+                    ["90"] = ("0.62", "1.33"),
+                    ["80"] = ("0.52", "1.12"),
+                    ["70"] = ("0.43", "0.93")
+                },
+
+                ["2560×1440"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["1920×1080"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["1366×768"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["1280×720"] = new() {
+                    ["120"] = ("1.30", "2.31"),
+                    ["110"] = ("1.07", "1.90"),
+                    ["100"] = ("0.89", "1.59"),
+                    ["90"] = ("0.75", "1.33"),
+                    ["80"] = ("0.63", "1.12"),
+                    ["70"] = ("0.53", "0.93")
+                },
+
+                ["640×480"] = new() {
+                    ["120"] = ("1.73", "2.31"),
+                    ["110"] = ("1.43", "1.90"),
+                    ["100"] = ("1.19", "1.59"),
+                    ["90"] = ("1.00", "1.33"),
+                    ["80"] = ("0.84", "1.12"),
+                    ["70"] = ("0.70", "0.93")
+                },
+
+            };
+
+            // Получаем значения
+            string selectedResolution = GUI_combo_screen.Text;
+            string selectedFov = GUI_combo_fov.Text;
+
+            // Проверяем, находим и ищем два значения в словаре
+            if (fovValues.TryGetValue(selectedResolution, out var resolutionFovs) &&
+                resolutionFovs.TryGetValue(selectedFov, out var fov))
+            {
+                
+                // Устанавливаем значения из словаря
+                fovTop = fov.fovTop; fovLeft = fov.fovLeft;
+
+                try
+                {
+                    // Читаем все строки из файла
+                    string[] lines = File.ReadAllLines(global_arma3profile_file_path);
+
+                    // Найденные строчки
+                    bool fovLeftFound = false; bool fovTopFound = false;
+
+                    // Проходим по всем строкам и ищем нужные параметры
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (lines[i].StartsWith("fovLeft="))
+                        {
+                            lines[i] = $"fovLeft={fovLeft}";
+                            fovLeftFound = true;
+                        }
+                        else if (lines[i].StartsWith("fovTop="))
+                        {
+                            lines[i] = $"fovTop={fovTop}";
+                            fovTopFound = true;
+                        }
+
+                        // Если оба параметра найдены, можно выйти из цикла
+                        if (fovLeftFound && fovTopFound)
+
+                            // Выход из цикла
+                            break;
+                    }
+
+                    // Если не все параметры найдены
+                    if (!fovLeftFound || !fovTopFound)
+                    {
+                        List<string> linesList = new List<string>(lines);
+                        if (!fovLeftFound)
+
+                            // Добавляем новые значения
+                            linesList.Add($"fovLeft={fovLeft}");
+
+                        if (!fovTopFound)
+
+                            // Добавляем новые значения
+                            linesList.Add($"fovTop={fovTop}");
+
+                        // Все строки файла
+                        lines = linesList.ToArray();
+                    }
+
+                    // Записываем изменения обратно в файл
+                    File.WriteAllLines(global_arma3profile_file_path, lines);
+
+                    // Выходим из функции
+                    return;
+
+                } catch (Exception ex) {
+
+                    // Уведомление пользователя
+                    MessageBox.Show($"При записи новых значений для FOV в выбранный вами профиль произошла ошибка (Вероятнее всего вы не закрыли игру или файл профиля занят другой программой): {ex.Message}", "Ошибка записи", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+
+
+            } else {
+
+                // Обработка случая, когда разрешение или FOV не найдены
+                MessageBox.Show("К сожалению программа не нашла скалькулированных данных по вашим параметрам, приношу извинения", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+                // Выходим из функции
+                return;
+            }
+
+        }
+
+        // Сохранение результата в файл профиля
+        private async void GUI_btn_save_Click(object sender, EventArgs e)
+        {
+
+            // Делаем проверку на то, что пользователь выбрал нужные параметры
+            if (GUI_combo_fov.SelectedIndex != -1 && GUI_combo_screen.SelectedIndex != -1)
+            {
+
+                // Если включено резервное копирование на рабочий стол
+                if (GUI_checkbox_bakup.Checked)
+                {
+
+                    // Получаем путь к рабочему столу текущего пользователя
+                    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                    // Формируем путь для копии файла на рабочем столе
+                    string destinationFilePath = Path.Combine(desktopPath, Path.GetFileName(global_arma3profile_file_path));
+
+                    // Копируем файл
+                    File.Copy(global_arma3profile_file_path, destinationFilePath, overwrite: true);
+
+                }
+
+                // Калькулируем и сохраняем файл
+                await calculateAndSave(global_arma3profile_file_path);
+
+                // Запоминаем текст
+                string savebtn_text = GUI_btn_save.Text;
+
+                // Делаем ее неактивной
+                GUI_btn_save.Enabled = false;
+
+                // Устанавливаем текст
+                GUI_btn_save.Text = "✔️ Успешно!";
+
+                // Ждем полторы секунды
+                await Task.Delay(1500);
+
+                // Возвращаем активность
+                GUI_btn_save.Enabled = Enabled;
+
+                // Возвращаем текст
+                GUI_btn_save.Text = savebtn_text;
+
+            } else {
+
+                // Уведомление пользователю
+                MessageBox.Show("К сожалению вы не выбрали все параметры, пожалуйста выберите", "Недостаточно параметров", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+        }
+
+        // Когда форма появилась
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+            // Проверка, пустое ли значение или нет
+            if (!string.IsNullOrEmpty(Properties.Settings.Default.savedPath))
+            {
+
+                // Получаем прошлый путь
+                string pathWithSettings = Properties.Settings.Default.savedPath;
+
+                // Проверяем все еще существование файла профиля
+                if (File.Exists(Properties.Settings.Default.savedPath))
+                {
+
+                    // Указываем новый путь из хранилища для профиля
+                    global_arma3profile_file_path = Properties.Settings.Default.savedPath;
+
+                    // Меняем текст выбора файла
+                    GUI_profile_label.Text = Uri.UnescapeDataString(Path.GetFileName(global_arma3profile_file_path));
+
+                    // Разблокировка параметров
+                    GUI_groupbox.Enabled = Enabled;
+
+                }
+
+                try
+                {
+
+                    // Установка FOV
+                    GUI_combo_fov.SelectedIndex = Properties.Settings.Default.savedFov;
+
+                    // Установка разрешения
+                    GUI_combo_screen.SelectedIndex = Properties.Settings.Default.savedScreen;
+
+                }
+                catch
+                {
+
+                    // Установка FOV
+                    GUI_combo_fov.SelectedIndex = 0;
+
+                    // Установка разрешения
+                    GUI_combo_screen.SelectedIndex = 0;
+
+                }
+
+
+            }
+
+
+        }
+
+        // Выбор элемента в Combobox (в двух), событие
+        private void ComboboxSelectIndex(object sender, EventArgs e)
+        {
+
+            // Запись в хранилище
+            Properties.Settings.Default.savedScreen = GUI_combo_screen.SelectedIndex;
+            Properties.Settings.Default.savedFov = GUI_combo_fov.SelectedIndex;
+
+            // Сохраняем
+            Properties.Settings.Default.Save();
+
+        }
+
+        // Нажатие на посетить GitHub
+        private void GUI_label_link_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            // Переходим на сайт
+            Process.Start("explorer", "https://github.com/AntonMalezhik");
+
+        }
+
+        // Нажатие на посетить Itch.io
+        private void GUI_label_link_1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+            // Переходим на веб сайт
+            Process.Start("explorer", "https://joblab-studio.itch.io");
+
+        }
+    }
+}
